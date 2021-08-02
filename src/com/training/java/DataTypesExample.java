61bde387b96752641fba849e563a35e902f576ba
Chinna.Putha <chinnareddy.putha@gmail.com>
2021-08-02 07:40:51 +0530

    java sample program

---
+++ src/com/training/java/DataTypesExample.java
@@ -0,0 +1,24 @@
+package com.training.java;
+
+public class DataTypesExample {
+	public static void main(String[] args) {
+		byte b = 0;
+		short s = 0;
+		int i = 0;
+		long l = 0;
+		float f = 0.0f;
+		double d = 0.0f;
+		char c = 0;
+		boolean bo = false;
+		System.out.println("Default value of byte " + b);
+		System.out.println("Default value of short " + s);
+		System.out.println("Default value of int " + i);
+		System.out.println("Default value of long " + l);
+		System.out.println("Default value of float " + f);
+		System.out.println("Default value of double " + d);
+		System.out.println("Default value of char " + c);
+		System.out.println("Default value of boolean " + bo);
+
+	}
+
+}
