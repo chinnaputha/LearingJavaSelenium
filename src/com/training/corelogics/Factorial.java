2af9394fb74cd7c652c3816a3ff9d15e4f91d07c
Vishnu-Tej <pasuparthi.vishnu@gmail.com>
2021-08-07 23:01:35 +0530

    adding Factorial program

---
+++ src/com/training/corelogics/Factorial.java
@@ -0,0 +1,21 @@
+package com.training.corelogics;
+
+/**
+ * 
+ * @author ACER
+ *
+ */
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int i;
+		int fact = 1;
+		int number = 6;
+		for (i = 1; i <= number; i++) {
+			fact = fact * i;
+		}
+		System.out.println("Factorial of " + number + " is: " + fact);//o/p is : 120
+	}
+
+}
