d91c1ebd5edb1f39c4d1bd1a97740cd479a8add6
BHARGAV <bharagavsivas@gmail.com>
2021-08-10 08:44:17 +0530

    prime number program

---
+++ src/com/training/corelogics/PrimeNumber.java
@@ -0,0 +1,32 @@
+package com.training.corelogics;
+public class PrimeNumber
+{
+	
+	public void prime(int num)
+	{
+	    boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i)
+	    	
+	    {
+	      // condition for nonprime number
+	      if (num % i == 0) 
+	      {
+	        flag =  true;
+	        break;
+	      }
+	    }
+	    if (!flag)
+            System.out.println(num + " is a prime number.");
+          else
+            System.out.println(num + " is not a prime number.");
+	   }
+	
+	public static void main(String[] args) 
+	{
+		PrimeNumber pn = new PrimeNumber();
+		pn.prime(10);
+		pn.prime(5);
+		pn.prime(1000);
+	}
+	
+}
