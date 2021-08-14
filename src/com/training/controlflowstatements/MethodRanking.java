2ece641c6b9d18225c8699b1077108c6b715c4f4
kssdprasad <kssdprasad@gmail.com>
2021-08-14 19:39:34 +0530

    MethodRanking

---
+++ src/com/training/controlflowstatements/MethodRanking.java
@@ -0,0 +1,21 @@
+package com.training.controlflowstatements;
+
+public class MethodRanking {
+	
+	public static void methodRankPoints(double points) {
+		if (points >= 202.5) {
+			System.out.println("Rank:A1");
+		} else if (points >= 122.4) {
+			System.out.println("Rank:A2");
+		} else {
+			System.out.println("Rank:A3");
+		}
+	}
+
+
+	public static void main(String[] args) {
+		methodRankPoints(225.22);
+
+	}
+
+}
