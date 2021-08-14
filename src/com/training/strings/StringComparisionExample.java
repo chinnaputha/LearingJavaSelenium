b75f8d91d268a0074f5a64857eaa8235408b3b11
Chinna.Putha <chinnareddy.putha@gmail.com>
2021-08-14 08:52:47 +0530

    String examples

---
+++ src/com/training/strings/StringComparisionExample.java
@@ -0,0 +1,19 @@
+package com.training.strings;
+
+public class StringComparisionExample {
+
+	public static void main(String[] args) {
+		//Strings are immutable that means whose content can’t be changed.
+		//Once string object is created its content can’t be changed but new object is created.
+
+		String s = "Java";//String literal -- String constant pool
+		System.out.println("s value -->"+s);
+		//s= s.concat("Selenium");// concat() method appends the string at the end
+		//System.out.println("s value-->"+s);// will print java because strings are immutable objects
+		
+		
+		s=s.replace("Java", "Selenium1");
+		System.out.println(s);
+	}
+
+}
