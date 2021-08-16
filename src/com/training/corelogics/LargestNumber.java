73bb4529c3e7cb7e336ce6ab9e685abb69d5a6dc
chinnareddy.putha <chinnareddy.putha@gmail.com>
2021-08-16 22:31:51 +0530

    Program push

--- src/com/training/corelogics/LargestNumber.java
+++ src/com/training/corelogics/LargestNumber.java
@@ -5,7 +5,7 @@ public class LargestNumber {
 	public static void main(String[] args) {
 		int arr[]= {1,67,9,88,785,199,434,1235};
 		int max=arr[0];
-		for(int i=1;i<arr.length;i++) {
+		for(int i=1;i<arr.length;i++) { 
 			if(max<arr[i]) { 
 				max=arr[i];   
 			}  
