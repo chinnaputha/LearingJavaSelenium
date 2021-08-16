52cb4dbf26b80a4c6582281ce3ed7dac60130fdf
Chinna.Putha <chinnareddy.putha@gmail.com>
2021-08-16 06:48:47 +0530

    Code commit

--- src/com/training/corelogics/LargestNumber.java
+++ src/com/training/corelogics/LargestNumber.java
@@ -7,7 +7,7 @@ public class LargestNumber {
 		int max=arr[0];
 		for(int i=1;i<arr.length;i++) {
 			if(max<arr[i]) {
-				max=arr[i];
+				max=arr[i]; 
 			}
 		}
 		System.out.println("Largest number-->"+max);
